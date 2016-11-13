2157490c4c1d6f664ed67d1bd4321e56b26ec282
Kelley <kelley@BOSKELLEYMBP.local>
2016-11-13 14:10:18 -0500

    added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# helloworld
+#
+
+# load library modules
+#
+import os
+import sys
+
+def main():
+  #
+  print("Hello world!")
+    
+
+print('__name__=' + __name__)
+if __name__ == "__main__":
+    main()
+
+                                                        
