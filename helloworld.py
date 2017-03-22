96761f3e7213ebf5d2156b31b8b752a68b46d6f0
bk <bkelley77@gmail.com>
2017-03-22 18:04:13 -0400

    added hostname for remote testing

--- helloworld.py
+++ helloworld.py
@@ -9,8 +9,10 @@ import sys
 
 def main():
   #
+  myhost = os.uname()[1]
   print("Hello world!")
-  print("Testing git with aws and pycharm paid for version8")
+  print("On host: " + myhost)
+  print("Testing git with aws and pycharm paid for version 12")
     
 print('__name__=' + __name__)
 if __name__ == "__main__":
